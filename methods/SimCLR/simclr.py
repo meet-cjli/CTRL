fc15b43282c38c2628b9a5f6928a7e44ccda9c35
Changjiang <cj_li@zju.edu.cn>
2023-01-30 11:13:39 -0500

    update

--- methods/SimCLR/simclr.py
+++ methods/SimCLR/simclr.py
@@ -54,24 +54,3 @@ class SimCLRModel(CLModel):
         features = torch.cat([f1.unsqueeze(1), f2.unsqueeze(1)], dim=1)
 
         return features
-    #
-    # def distill(self, v1, v2):
-    #     with torch.no_grad():
-    #         rep1 = self.backbone(v1)
-    #
-    #     rep2 = self.distill_backbone(v2)
-    #
-
-
-
-    # @torch.no_grad()
-    # def _momentum_update_key_encoder(self):
-    #     """
-    #     Momentum update of the key encoder
-    #     """
-    #     for param_q, param_k in zip(self.encoder_q.parameters(), self.encoder_k.parameters()):
-    #         param_k.data = param_k.data * self.m + param_q.data * (1. - self.m)
-
-
-
-
